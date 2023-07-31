79465571146da0ba639deb496bab754873e5bfac
Anton Yevmenov <58602896+AntonYevmenov94@users.noreply.github.com>
2023-07-31 20:58:03 +0300

    Add files via upload

---
+++ EndpointSecurityManager/EndpointSecurityManager/EndpointManager.swift
@@ -0,0 +1,45 @@
+import Foundation
+import os.log
+import SystemExtensions
+
+class EndpointManager: NSObject, OSSystemExtensionRequestDelegate {
+    
+    // Здесь укажите идентификатор вашего расширения
+    let extensionIdentifier = "ccom.cmpny.EndpointSecurityManager.Extension"
+    
+    func installExtension() {
+        os_log("Start to install ", log: .default, type: .info)
+        let request = OSSystemExtensionRequest.activationRequest(forExtensionWithIdentifier: extensionIdentifier, queue: .main)
+        request.delegate = self
+        OSSystemExtensionManager.shared.submitRequest(request)
+        
+    }
+    
+    func uninstallExtension() {
+        let request = OSSystemExtensionRequest.deactivationRequest(forExtensionWithIdentifier: extensionIdentifier, queue: .main)
+        request.delegate = self
+        OSSystemExtensionManager.shared.submitRequest(request)
+    }
+    
+    // MARK: - OSSystemExtensionRequestDelegate
+    
+    func request(_ request: OSSystemExtensionRequest, actionForReplacingExtension existing: OSSystemExtensionProperties, withExtension ext: OSSystemExtensionProperties) -> OSSystemExtensionRequest.ReplacementAction {
+        os_log("Replacing extension: %@ %@", existing, ext);
+        return .replace;
+    }
+    
+    func requestNeedsUserApproval(_ request: OSSystemExtensionRequest) {
+        os_log("System extension requires user approval");
+    }
+    
+    func request(_ request: OSSystemExtensionRequest, didFinishWithResult result: OSSystemExtensionRequest.Result) {
+        os_log("System extension activaiting request result: %d", result.rawValue);
+    }
+    
+    func request(_ request: OSSystemExtensionRequest, didFailWithError error: Error) {
+        os_log("System extension request failed %@", error.localizedDescription);
+    }
+}
+
+
+
